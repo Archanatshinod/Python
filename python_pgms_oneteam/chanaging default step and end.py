adced2a47b695191aadb4d9ecaad1e44dd673ac5
Archana T <116487133+Archanatshinod@users.noreply.github.com>
2024-06-23 12:23:26 +0530

    Add files via upload

---
+++ python_pgms_oneteam/chanaging default step and end.py
@@ -0,0 +1,24 @@
+print("archana","athulya","anagha")
+
+# changing default separator
+print("archana","athulya","anagha",sep=",")
+
+print("archana","athulya","anagha",sep="->")
+
+print("archana","athulya","anagha",sep="-")
+
+print("archana","athulya","anagha",sep="\n")
+
+#changing default end
+print("anagha")
+print("darasana")
+print("anusha")
+
+print("anagha",end=":")
+print("darasana",end="-")
+print("anusha",end=">")
+print("\n \n \n now changed format:")
+
+######
+print("archana","athulya","anagha",sep=",",end="->")
+print(22,23,45)
